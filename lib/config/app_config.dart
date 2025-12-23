5e54ed0760f89b40792b168dedf09f7b4232b7ba
Massimo Lo Sciuto <losciuto@hotmail.com>
2025-12-23 20:40:53 +0100

    Update version in AppConfig to 1.3.0

--- lib/config/app_config.dart
+++ lib/config/app_config.dart
@@ -1,7 +1,7 @@
 /// Configurazione dell'applicazione VLC Remote
 class AppConfig {
   // Versione dell'app
-  static const String appVersion = '1.2.1';
+  static const String appVersion = '1.3.0';
   static const String appBuildNumber = '1';
 
   // Impostazioni di connessione
